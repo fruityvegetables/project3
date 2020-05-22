363e454c181e21445fa5b88c1587b032d32f16f3
Kyle PC <kyle.dahl201@gmail.com>
2020-05-21 20:03:59 -0500

    add plant form component

---
+++ src/components/AddPlantForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class AddPlantForm extends React.Component {
+
+    render() {
+        return (
+            <form>
+                <p>
+                    it works dawg
+                </p>
+            </form>
+           
+        );
+    }
+
+}  
+
+export default AddPlantForm;
