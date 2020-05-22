4627e37c505353b872e9fe9eadb3d0cf8aaa02cd
Kyle PC <kyle.dahl201@gmail.com>
2020-05-22 02:13:26 -0500

    adding state to App.js for AddPlant

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,22 @@ import Inventory from "./Inventory";
 
 
 class App extends React.Component {
+    //setting state from AddPlantForm
+    state = {
+        plants: {},
+        gardenplants: {}
+    }
+    addPlant = plant => {
+       console.log("adding a plant") 
+       //step one take copy of existing state 
+       const plants = {...this.state.plants}
+       //add new plant to plants const
+       plants[`plant${Date.now()}`] = plant
+       //put eupdated state into state, set new plants objects to state
+       this.setState({
+           plants: plants
+       })
+    }
     render() {
             return(
                 <div className="catch-of-the-day">
@@ -14,7 +30,7 @@ class App extends React.Component {
                         
                     </div>
                     <MyGarden />
-                    <Inventory />
+                    <Inventory addPlant={this.addPlant} />
                  
                 </div>
             )
