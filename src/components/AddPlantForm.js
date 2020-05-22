ce348e118a79a97dd14dd7510d68b395eed3765d
Kyle PC <kyle.dahl201@gmail.com>
2020-05-22 01:20:49 -0500

    add plant form edit

--- src/components/AddPlantForm.js
+++ src/components/AddPlantForm.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 class AddPlantForm extends React.Component {
-
+    //for onSubmit
+    createPlant = (event) => {
+        //stop form from submitting
+        event.preventDefault();
+        console.log("GROWING GREEN GREEN PLANTS 🌱 ")
+    }
     render() {
         return (
-            <form>
-                <p>
-                    it works dawg
-                </p>
+            <form className="plant-edit" onSubmit={this.createPlant}>
+                    <input name="name" type="text" placeholder="Name" /> 
+                    <input name="price" type="text" placeholder="Price" /> 
+                    <select name="status">
+                        <option value="Available">Available!</option>
+                        <option value="Unavailable">Sold Out</option>
+                    </select>
+
+                    <textarea name="desc" placeholder="Desc" /> 
+                    <input name="image" type="text" placeholder="Image" /> 
+                    <button type="submit">+Add Plant</button>
             </form>
            
         );
